42b6611b080890cb04fd968b3ff37e7fcb4c22b4
qianbing <1532498760@qq.com>
2020-12-24 16:51:23 +0800

    '11'

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ function resolve(dir) {
     return path.join(__dirname, dir);
 }
 module.exports = {
-    lintOnSave: true,
+    lintOnSave: false,
     chainWebpack: (config) => {
         config.resolve.alias
             .set('@', resolve('src'))
